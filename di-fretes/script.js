370d40f07855fb6bbad1264d7930e3115871ca04
dinei84 <dinei.pita@gmail.com>
2023-10-05 23:55:04 -0300

    Segundo Commit da Difrete

--- di-fretes/script.js
+++ di-fretes/script.js
@@ -2,22 +2,33 @@ document.addEventListener('DOMContentLoaded', function () {
     const form = document.getElementById('freteForm');
 
     form.addEventListener('submit', function (e) {
-        e.preventDefault();
+        e.preventDefault(); // Evita o envio padrão do formulário
 
-        const frete = document.getElementById('frete').value;
-        const carga = document.getElementById('carga').value;
-        const descarga = document.getElementById('descarga').value;
+        const uf = document.getElementById('uf').value;
+        const destino = document.getElementById('destino').value;
+        const embalagem = document.getElementById('embalagem').value;
+        const coleta = document.getElementById('coleta').value;
+        const entrega = document.getElementById('entrega').value;
+        const cliente = document.getElementById('cliente').value;
+        const produto = document.getElementById('produto').value;
+        const infoAdicionais = document.getElementById('infoAdicionais').value;
 
         // Obtenha todas as entradas existentes de frete do localStorage
         const fretes = JSON.parse(localStorage.getItem('fretes')) || [];
 
         // Adicione a nova entrada de frete à lista
-        fretes.push({ frete, carga, descarga });
+        fretes.push({ 
+            uf, 
+            destino, 
+            embalagem, 
+            coleta, 
+            entrega, 
+            cliente, 
+            produto, 
+            infoAdicionais 
+        });
 
         // Armazene a lista atualizada no localStorage
         localStorage.setItem('fretes', JSON.stringify(fretes));
-
-        // Atualize a página atual
-        window.location.reload();
     });
 });
